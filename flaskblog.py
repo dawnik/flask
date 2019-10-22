81e59e911c5cff25b44aafaf8874813b25c5e0c6
dawnik <dawnik@dawnik>
2019-10-22 14:24:23 +0200

    create basic layout.html for pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,34 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+#simple blok post
+posts = [
+    {
+        'author': 'Corey',
+        'title': 'Blog post 1',
+        'content': 'First post content, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor '
+                   'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
+                   'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in '
+                   'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non '
+                   'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'data_postsed': 'April 20, 2019'
+    },
+    {
+        'author': 'Dominik',
+        'title': 'Blog post 2',
+        'content': 'Second post content, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do tempor '
+                   'incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation '
+                   'ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in '
+                   'voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non '
+                   'proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'data_postsed': 'April 20, 2019'
+    }
+]
+
+@app.route('/home')
+def home():
+    return render_template('home.html',  title='Home', posts=posts)
+
 @app.route('/about')
-def index():
-    return render_template('about.html')
+def about():
+    return render_template('about.html', title='About')
